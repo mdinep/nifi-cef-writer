ac51e750c0c045e1c0cd8bffd8b1398d9cfafac7
Matt Dinep <matt.dinep@gmail.com>
2024-02-13 20:28:54 -0800

    Added properties and method to format text by escaping unallowed characters and checking for already escaped slashes

--- nifi-cef-processors/src/main/java/com/cloudera/processors/cef/CefWriter.java
+++ nifi-cef-processors/src/main/java/com/cloudera/processors/cef/CefWriter.java
@@ -17,6 +17,8 @@
 package com.cloudera.processors.cef;
 
 import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.expression.AttributeExpression;
+import org.apache.nifi.expression.ExpressionLanguageScope;
 import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.annotation.behavior.ReadsAttribute;
 import org.apache.nifi.annotation.behavior.ReadsAttributes;
@@ -33,11 +35,7 @@ import org.apache.nifi.processor.ProcessorInitializationContext;
 import org.apache.nifi.processor.Relationship;
 import org.apache.nifi.processor.util.StandardValidators;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Tags({"example"})
 @CapabilityDescription("Provide a description")
@@ -46,13 +44,79 @@ import java.util.Set;
 @WritesAttributes({@WritesAttribute(attribute="", description="")})
 public class CefWriter extends AbstractProcessor {
 
-    public static final PropertyDescriptor MY_PROPERTY = new PropertyDescriptor
-            .Builder().name("MY_PROPERTY")
-            .displayName("My property")
-            .description("Example Property")
+    public static final PropertyDescriptor SYSLOG_PREFIX = new PropertyDescriptor
+            .Builder().name("SYSLOG_PREFIX")
+            .displayName("Syslog Prefix")
+            .description("The first field of a CEF object. Example: CEF:1")
             .required(true)
+            .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
             .build();
+    public static final PropertyDescriptor DEVICE_VENDOR = new PropertyDescriptor
+            .Builder().name("DEVICE_VENDOR")
+            .displayName("Device Vendor")
+            .description("The second field of a CEF object. Example: Security")
+            .required(true)
+            .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+    public static final PropertyDescriptor DEVICE_PRODUCT = new PropertyDescriptor
+            .Builder().name("DEVICE_PRODUCT")
+            .displayName("Device Product")
+            .description("The third field of a CEF object. Example: threatmanager")
+            .required(true)
+            .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+    public static final PropertyDescriptor DEVICE_VERSION = new PropertyDescriptor
+            .Builder().name("DEVICE_VERSION")
+            .displayName("Device Version")
+            .description("The fourth field of a CEF object. Example: 1.0")
+            .required(true)
+            .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+    public static final PropertyDescriptor DEVICE_CLASS_ID = new PropertyDescriptor
+            .Builder().name("DEVICE_CLASS_ID")
+            .displayName("Device Event Class ID")
+            .description("The fifth field of a CEF object. Example: 100")
+            .required(true)
+            .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+    public static final PropertyDescriptor EVENT_NAME = new PropertyDescriptor
+            .Builder().name("EVENT_NAME")
+            .displayName("Event Name")
+            .description("The sixth field of a CEF object. Example: worm successfully stopped")
+            .required(true)
+            .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+    public static final PropertyDescriptor EVENT_SEVERITY = new PropertyDescriptor
+            .Builder().name("EVENT_SEVERITY")
+            .displayName("Event Severity")
+            .description("The seventh field of a CEF object. Example: 10")
+            .required(true)
+            .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+    public static final PropertyDescriptor EVENT_DATE = new PropertyDescriptor
+            .Builder().name("EVENT_DATE")
+            .displayName("Event DateTime")
+            .description("Optional datetime value that can be part of the Header. Example: Sept 29 08:26:10")
+            .required(false)
+            .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+    public static final PropertyDescriptor EVENT_HOST = new PropertyDescriptor
+            .Builder().name("EVENT_HOST")
+            .displayName("Event Host")
+            .description("Optional hostname value that can be part of the Header. Example: host.example.com")
+            .required(false)
+            .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+
 
     public static final Relationship ORIGINAL = new Relationship.Builder()
             .name("Original")
@@ -70,11 +134,20 @@ public class CefWriter extends AbstractProcessor {
 
     private List<PropertyDescriptor> properties;
     private Set<Relationship> relationships;
+    private volatile boolean stateful = false;
 
     @Override
     protected void init(final ProcessorInitializationContext context) {
         properties = new ArrayList<>();
-        properties.add(MY_PROPERTY);
+        properties.add(SYSLOG_PREFIX);
+        properties.add(DEVICE_VENDOR);
+        properties.add(DEVICE_PRODUCT);
+        properties.add(DEVICE_VERSION);
+        properties.add(DEVICE_CLASS_ID);
+        properties.add(EVENT_SEVERITY);
+        properties.add(EVENT_DATE);
+        properties.add(EVENT_HOST);
+
         properties = Collections.unmodifiableList(properties);
 
         relationships = new HashSet<>();
@@ -92,6 +165,44 @@ public class CefWriter extends AbstractProcessor {
     @Override
     public final List<PropertyDescriptor> getSupportedPropertyDescriptors() {
         return properties;
+    }
+
+    /*
+    @Override
+    protected PropertyDescriptor getSupportedDynamicPropertyDescriptor(final String propertyDescriptorName) {
+        PropertyDescriptor.Builder propertyBuilder = new PropertyDescriptor.Builder()
+                .name(propertyDescriptorName)
+                .required(false)
+                .addValidator(StandardValidators.ATTRIBUTE_KEY_PROPERTY_NAME_VALIDATOR)
+                .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+                .dynamic(true);
+
+        if (stateful) {
+            return propertyBuilder
+                    .addValidator(StandardValidators.createAttributeExpressionLanguageValidator(AttributeExpression.ResultType.STRING, true))
+                    .build();
+        } else {
+            return propertyBuilder
+                    .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+                    .build();
+        }
+    }
+    */
+
+    public final String formatText(String inputString){
+        StringBuilder outputString = new StringBuilder();
+        for(int i=0;i < inputString.length();i++){
+            if (String.valueOf(inputString.charAt(i)).equals("=") || String.valueOf(inputString.charAt(i)).equals("|")){
+                outputString.append("\\").append(inputString.charAt(i));
+            } else if (String.valueOf(inputString.charAt(i)).equals("\\")
+                    && !String.valueOf(inputString.charAt(i-1)).equals("\\")
+                    && !String.valueOf(inputString.charAt(i+1)).equals("\\")){
+                outputString.append("\\").append(inputString.charAt(i));
+            } else {
+                outputString.append(inputString.charAt(i));
+            }
+        }
+        return outputString.toString();
     }
 
     @OnScheduled
