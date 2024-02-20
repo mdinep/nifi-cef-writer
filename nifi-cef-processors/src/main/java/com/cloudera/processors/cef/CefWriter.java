1a3a43759ce4747c48c1525679a7749d3a794aae
Matt Dinep <matt.dinep@gmail.com>
2024-02-20 15:57:04 -0800

    Removed original relationship, enabled dynamic properties, and finished OnTrigger method

--- nifi-cef-processors/src/main/java/com/cloudera/processors/cef/CefWriter.java
+++ nifi-cef-processors/src/main/java/com/cloudera/processors/cef/CefWriter.java
@@ -16,14 +16,11 @@
  */
 package com.cloudera.processors.cef;
 
+import org.apache.nifi.annotation.behavior.*;
 import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.expression.AttributeExpression;
 import org.apache.nifi.expression.ExpressionLanguageScope;
 import org.apache.nifi.flowfile.FlowFile;
-import org.apache.nifi.annotation.behavior.ReadsAttribute;
-import org.apache.nifi.annotation.behavior.ReadsAttributes;
-import org.apache.nifi.annotation.behavior.WritesAttribute;
-import org.apache.nifi.annotation.behavior.WritesAttributes;
 import org.apache.nifi.annotation.lifecycle.OnScheduled;
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
 import org.apache.nifi.annotation.documentation.SeeAlso;
@@ -33,24 +30,62 @@ import org.apache.nifi.processor.ProcessContext;
 import org.apache.nifi.processor.ProcessSession;
 import org.apache.nifi.processor.ProcessorInitializationContext;
 import org.apache.nifi.processor.Relationship;
+import org.apache.nifi.processor.io.InputStreamCallback;
+import org.apache.nifi.processor.io.OutputStreamCallback;
 import org.apache.nifi.processor.util.StandardValidators;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.stream.Collectors;
 
-@Tags({"example"})
-@CapabilityDescription("Provide a description")
-@SeeAlso({})
-@ReadsAttributes({@ReadsAttribute(attribute="", description="")})
-@WritesAttributes({@WritesAttribute(attribute="", description="")})
+@EventDriven
+@InputRequirement(InputRequirement.Requirement.INPUT_REQUIRED)
+@Tags({"cef", "SIEM", "modification", "update", "Attribute Expression Language", "Arcsight"})
+@CapabilityDescription("Builds and outputs a CEF record using configured flowfile attributes or hard-coded values for use by Arcsight and other SIEM applications that utilize the CEF format")
+@DynamicProperty(name = "A FlowFile attribute used in the CEF Extension field", value = "The value to set it to", expressionLanguageScope = ExpressionLanguageScope.FLOWFILE_ATTRIBUTES,
+        description = "Dynamic Property names and values are used as the key/value of additional CEF fields stored in the CEF Extension field. These names must conform to the CEF schema or user-defined additional custom field names implemented in downstream applications")
+@WritesAttribute(attribute = "See additional details", description = "This processor may write or remove zero or more attributes as described in additional details")
 public class CefWriter extends AbstractProcessor {
 
+    //Restricted values for dropdown selection of whether complex header will be used
+    public static final String DO_NOT_USE_COMPLEX_HEADER = "Do not use complex header";
+    public static final String USE_COMPLEX_HEADERE = "Use complex header";
+    public static final String USE_CONTENT_AS_MSG = "Use flowfile content";
+    public static final String DO_NOT_USE_CONTENT_AS_MSG = "Do not use flowfile content";
+
+    //Complex header selection property
+    public static final PropertyDescriptor COMPLEX_HEADER = new PropertyDescriptor.Builder()
+            .name("USE_COMPLEX_HEADER")
+            .displayName("Use complex header")
+            .description("Use the complex header for the first CEF field. This consists of the event date time, event host, and cef version "+
+                    "separated by spaces. Setting this to true requires the optional properties 'Event Data' and 'Event Host' to be populated. "+
+                    "If these properties are not set, flowfiles will fail to process. NOTE: not all downstream applications are configured to "+
+                    "support complex headers in CEF records. Make sure your application can handle this before enabling this option.")
+            .required(true)
+            .allowableValues(DO_NOT_USE_COMPLEX_HEADER, USE_COMPLEX_HEADERE)
+            .defaultValue(DO_NOT_USE_COMPLEX_HEADER)
+            .build();
+
+    public static final PropertyDescriptor CONTENT_AS_MSG = new PropertyDescriptor.Builder()
+            .name("CONTENT_AS_MSG")
+            .displayName("Content as msg")
+            .description("Set the content of the flowfile as the value of the 'msg' property in the Extension field of the cef record"+
+                    "NOTE: depending on downstream requirements, this could result in a very large record that may not be supported. Use with caution.")
+            .required(true)
+            .allowableValues(USE_CONTENT_AS_MSG, DO_NOT_USE_CONTENT_AS_MSG)
+            .defaultValue(DO_NOT_USE_CONTENT_AS_MSG)
+            .build();
+
+    //Required properties for CEF record construction. Supports expression language
     public static final PropertyDescriptor SYSLOG_PREFIX = new PropertyDescriptor
             .Builder().name("SYSLOG_PREFIX")
             .displayName("Syslog Prefix")
             .description("The first field of a CEF object. Example: CEF:1")
             .required(true)
             .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
-            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            //.addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .addValidator(StandardValidators.createAttributeExpressionLanguageValidator(AttributeExpression.ResultType.STRING, true))
             .build();
     public static final PropertyDescriptor DEVICE_VENDOR = new PropertyDescriptor
             .Builder().name("DEVICE_VENDOR")
@@ -59,6 +94,7 @@ public class CefWriter extends AbstractProcessor {
             .required(true)
             .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .addValidator(StandardValidators.createAttributeExpressionLanguageValidator(AttributeExpression.ResultType.STRING, true))
             .build();
     public static final PropertyDescriptor DEVICE_PRODUCT = new PropertyDescriptor
             .Builder().name("DEVICE_PRODUCT")
@@ -67,6 +103,7 @@ public class CefWriter extends AbstractProcessor {
             .required(true)
             .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .addValidator(StandardValidators.createAttributeExpressionLanguageValidator(AttributeExpression.ResultType.STRING, true))
             .build();
     public static final PropertyDescriptor DEVICE_VERSION = new PropertyDescriptor
             .Builder().name("DEVICE_VERSION")
@@ -75,6 +112,7 @@ public class CefWriter extends AbstractProcessor {
             .required(true)
             .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .addValidator(StandardValidators.createAttributeExpressionLanguageValidator(AttributeExpression.ResultType.STRING, true))
             .build();
     public static final PropertyDescriptor DEVICE_CLASS_ID = new PropertyDescriptor
             .Builder().name("DEVICE_CLASS_ID")
@@ -83,6 +121,7 @@ public class CefWriter extends AbstractProcessor {
             .required(true)
             .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .addValidator(StandardValidators.createAttributeExpressionLanguageValidator(AttributeExpression.ResultType.STRING, true))
             .build();
     public static final PropertyDescriptor EVENT_NAME = new PropertyDescriptor
             .Builder().name("EVENT_NAME")
@@ -91,6 +130,7 @@ public class CefWriter extends AbstractProcessor {
             .required(true)
             .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .addValidator(StandardValidators.createAttributeExpressionLanguageValidator(AttributeExpression.ResultType.STRING, true))
             .build();
     public static final PropertyDescriptor EVENT_SEVERITY = new PropertyDescriptor
             .Builder().name("EVENT_SEVERITY")
@@ -99,7 +139,10 @@ public class CefWriter extends AbstractProcessor {
             .required(true)
             .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .addValidator(StandardValidators.createAttributeExpressionLanguageValidator(AttributeExpression.ResultType.STRING, true))
             .build();
+
+    //Optional properties for CEF records. Required if complex header is set to true
     public static final PropertyDescriptor EVENT_DATE = new PropertyDescriptor
             .Builder().name("EVENT_DATE")
             .displayName("Event DateTime")
@@ -107,6 +150,7 @@ public class CefWriter extends AbstractProcessor {
             .required(false)
             .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .addValidator(StandardValidators.createAttributeExpressionLanguageValidator(AttributeExpression.ResultType.STRING, true))
             .build();
     public static final PropertyDescriptor EVENT_HOST = new PropertyDescriptor
             .Builder().name("EVENT_HOST")
@@ -115,13 +159,26 @@ public class CefWriter extends AbstractProcessor {
             .required(false)
             .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .addValidator(StandardValidators.createAttributeExpressionLanguageValidator(AttributeExpression.ResultType.STRING, true))
             .build();
 
 
-    public static final Relationship ORIGINAL = new Relationship.Builder()
-            .name("Original")
-            .description("The original input flowfile")
-            .build();
+    //Codeblock for processor to support dynamic properties. Used to build key/value pairs for CEF extension field
+    @Override
+    protected PropertyDescriptor getSupportedDynamicPropertyDescriptor(final String propertyDescriptorName) {
+        return new PropertyDescriptor.Builder()
+                .name(propertyDescriptorName)
+                .required(false)
+                .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+                .addValidator(StandardValidators.ATTRIBUTE_KEY_PROPERTY_NAME_VALIDATOR)
+                .addValidator(StandardValidators.createAttributeExpressionLanguageValidator(AttributeExpression.ResultType.STRING, true))
+                .dynamic(true)
+                .build();
+    }
+
+
+
+    //Relationships to which flowfiles are transferred
     public static final Relationship SUCCESS = new Relationship.Builder()
             .name("Success")
             .description("The flowfile has been successfully processed to CEF")
@@ -134,16 +191,18 @@ public class CefWriter extends AbstractProcessor {
 
     private List<PropertyDescriptor> properties;
     private Set<Relationship> relationships;
-    private volatile boolean stateful = false;
 
     @Override
     protected void init(final ProcessorInitializationContext context) {
         properties = new ArrayList<>();
+        properties.add(COMPLEX_HEADER);
+        properties.add(CONTENT_AS_MSG);
         properties.add(SYSLOG_PREFIX);
         properties.add(DEVICE_VENDOR);
         properties.add(DEVICE_PRODUCT);
         properties.add(DEVICE_VERSION);
         properties.add(DEVICE_CLASS_ID);
+        properties.add(EVENT_NAME);
         properties.add(EVENT_SEVERITY);
         properties.add(EVENT_DATE);
         properties.add(EVENT_HOST);
@@ -151,7 +210,6 @@ public class CefWriter extends AbstractProcessor {
         properties = Collections.unmodifiableList(properties);
 
         relationships = new HashSet<>();
-        relationships.add(ORIGINAL);
         relationships.add(SUCCESS);
         relationships.add(FAILURE);
         relationships = Collections.unmodifiableSet(relationships);
@@ -167,28 +225,7 @@ public class CefWriter extends AbstractProcessor {
         return properties;
     }
 
-    /*
-    @Override
-    protected PropertyDescriptor getSupportedDynamicPropertyDescriptor(final String propertyDescriptorName) {
-        PropertyDescriptor.Builder propertyBuilder = new PropertyDescriptor.Builder()
-                .name(propertyDescriptorName)
-                .required(false)
-                .addValidator(StandardValidators.ATTRIBUTE_KEY_PROPERTY_NAME_VALIDATOR)
-                .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
-                .dynamic(true);
-
-        if (stateful) {
-            return propertyBuilder
-                    .addValidator(StandardValidators.createAttributeExpressionLanguageValidator(AttributeExpression.ResultType.STRING, true))
-                    .build();
-        } else {
-            return propertyBuilder
-                    .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
-                    .build();
-        }
-    }
-    */
-
+    //Method to check and fix dat formatting for CEF records. Ensures that unallowed control characters are properly escaped
     public final String formatText(String inputString){
         StringBuilder outputString = new StringBuilder();
         for(int i=0;i < inputString.length();i++){
@@ -205,17 +242,99 @@ public class CefWriter extends AbstractProcessor {
         return outputString.toString();
     }
 
-    @OnScheduled
-    public void onScheduled(final ProcessContext context) {
-
-    }
-
     @Override
     public void onTrigger(final ProcessContext context, final ProcessSession session) {
         FlowFile flowFile = session.get();
         if (flowFile == null) {
             return;
         }
-        // TODO implement
+
+        StringJoiner cefJ = new StringJoiner("|");
+        StringJoiner fieldJ = new StringJoiner(" ");
+        String cefPref = context.getProperty(SYSLOG_PREFIX).evaluateAttributeExpressions(flowFile).getValue();
+        String complexHeader = context.getProperty(COMPLEX_HEADER).evaluateAttributeExpressions(flowFile).getValue();
+        String contentMsg = context.getProperty(CONTENT_AS_MSG).evaluateAttributeExpressions(flowFile).getValue();
+
+        if(complexHeader.equals(USE_COMPLEX_HEADERE)) {
+            String datetime = context.getProperty(EVENT_DATE).evaluateAttributeExpressions(flowFile).getValue();
+            String eventHost = context.getProperty(EVENT_HOST).evaluateAttributeExpressions(flowFile).getValue();
+
+            String headerString = new StringJoiner(" ").add(datetime).add(eventHost).add(cefPref).toString();
+            try{
+                cefJ.add(formatText(headerString));
+            } catch (Exception ex){
+                ex.printStackTrace();
+                getLogger().error("Failed to Build complex CEF header.");
+                session.transfer(flowFile, FAILURE);
+            }
+
+        } else {
+            try{
+                cefJ.add(formatText(cefPref));
+            }catch (Exception ex){
+                ex.printStackTrace();
+                getLogger().error("Failed to write simple CEF header.");
+                session.transfer(flowFile, FAILURE);
+            }
+        }
+
+        try{
+            cefJ.add(formatText(context.getProperty(DEVICE_VENDOR).evaluateAttributeExpressions(flowFile).getValue()));
+            cefJ.add(formatText(context.getProperty(DEVICE_PRODUCT).evaluateAttributeExpressions(flowFile).getValue()));
+            cefJ.add(formatText(context.getProperty(DEVICE_VERSION).evaluateAttributeExpressions(flowFile).getValue()));
+            cefJ.add(formatText(context.getProperty(DEVICE_CLASS_ID).evaluateAttributeExpressions(flowFile).getValue()));
+            cefJ.add(formatText(context.getProperty(EVENT_NAME).evaluateAttributeExpressions(flowFile).getValue()));
+            cefJ.add(formatText(context.getProperty(EVENT_SEVERITY).evaluateAttributeExpressions(flowFile).getValue()));
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+            getLogger().error("Failed to add field to CEF record. Check to make sure all required fields have valid values.");
+            session.transfer(flowFile, FAILURE);
+        }
+
+        if(contentMsg.equals(USE_CONTENT_AS_MSG)){
+            session.read(flowFile, new InputStreamCallback() {
+                @Override
+                public void process(InputStream inputStream) {
+                    try {
+                        String text = new BufferedReader(
+                                new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+                                .lines()
+                                .collect(Collectors.joining(" "));
+                        fieldJ.add("msg=" + formatText(text));
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                        getLogger().error("Failed to read flowfile content.");
+                        session.transfer(flowFile, FAILURE);
+                    }
+                }
+            });
+        }
+
+        for ( Map.Entry< PropertyDescriptor, String > entry : context.getProperties().entrySet() ){
+            PropertyDescriptor dynamicProps = entry.getKey();
+            if(dynamicProps.isDynamic()) {
+                final String PROPERTY_NAME  = dynamicProps.getName();
+                final String PROPERTY_VALUE = formatText(context.getProperty(PROPERTY_NAME).evaluateAttributeExpressions(flowFile).getValue());
+                fieldJ.add(PROPERTY_NAME.strip() + "=" +PROPERTY_VALUE.strip());
+            }
+        }
+        cefJ.add(fieldJ.toString());
+        FlowFile cefF = session.create(flowFile);
+        cefF = session.write(cefF, new OutputStreamCallback() {
+            @Override
+            public void process(OutputStream outputStream) throws IOException {
+                try{
+                    outputStream.write(cefJ.toString().getBytes());
+                }catch(Exception ex){
+                    ex.printStackTrace();
+                    getLogger().error("Failed to write CEF record to new flowfile content.");
+                    session.transfer(flowFile, FAILURE);
+                }
+            }
+        });
+
+        session.remove(flowFile);
+        session.transfer(cefF, SUCCESS);
     }
 }
